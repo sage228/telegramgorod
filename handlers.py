f87b259a5748b03222349008718decc417b6b016
sage228 <87944907+sage228@users.noreply.github.com>
2021-12-23 20:06:51 +0300

    Add files via upload

---
+++ handlers.py
@@ -0,0 +1,14 @@
+from bot import bot, dp
+from aiogram.types import Message
+from config import admin_id, users
+import time
+from selenium import webdriver
+
+
+async def send_to_admin(dp):
+    await bot.send_message(chat_id=admin_id, text='Бот запущен')
+
+# Страница
+#driver = webdriver.Chrome()
+#driver.get('https://vk.com/rick.n.morty')
+
